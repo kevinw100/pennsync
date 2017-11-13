5342602276c95178ebbb57307b5a3d821d8e62e3
Kyle Su <sukyle@seas.upenn.edu>
2017-11-13 17:40:38 -0500

    Problems resolving jassh dependency

--- build.sbt
+++ build.sbt
@@ -3,4 +3,6 @@ name := "PennSync"
 version := "1.0"
 
 scalaVersion := "2.12.1"
+
+libraryDependencies += "fr.janalyse" %% "janalyse-ssh" % "0.10.3"
         
--- src/main/scala/com/pennsync/Main.scala
+++ src/main/scala/com/pennsync/Main.scala
@@ -2,10 +2,18 @@ package com.pennsync
 
 import java.io.File
 
+import fr.janalyse.ssh.{SSH, SSHOptions}
+
 object Main extends App {
   if (args.length != 2) {
     println("I need a file name and an ip address doofus!")
   } else {
+    val sshOpt: SSHOptions = SSHOptions("10.215.150.241", "pi", "pi")
+
+    implicit val connec: SSH = new SSH(sshOpt)
+
+    println(connec.pwd)
+
     println(args(0))
     println(args(1))
 
